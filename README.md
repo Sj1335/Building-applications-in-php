a900b103ea5bfe55cee43a51799ff03588f74538
Shagun <shagunjindal012@gmail.com>
2022-01-17 19:25:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Building Web Applications In PHP
+This repository contains notes, assignments, quizzes and code files from the "Building Web Applications in PHP" course by University of Michigan, on Coursera.
+
+Find the course at https://www.coursera.org/learn/web-applications-php
---
+++ week5 php/index.php
@@ -0,0 +1,19 @@
+
+<title>Shagun Jindal PHP</title>
+<h1>Shagun Jindal PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Shagun\" is ".hash('sha256', 'Shagun');
+?></p>
+<pre>ASCII ART:
+	      *********
+	      * *******
+	      *	*  
+	      * *******
+	      *********
+	            * *
+	      ******* *
+	      *********
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
